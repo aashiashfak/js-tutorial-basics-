952b8056e2a9cc12e606199561ac39fb779e50dc
aashiashfak <aashiashfak@gmail.com>
2024-05-09 20:56:12 +0530

    updated knowledge

--- functions.js
+++ functions.js
@@ -35,3 +35,41 @@ x()
 x()
 x()
 
+// call back 
+
+function fetchedData(callback) {
+    setTimeout(() => {
+      const data = "This is the fetched data";
+      callback(data); // Execute the callback function with the fetched data
+    }, 2000);
+}
+
+function processData(data) {
+    console.log('processed data:', data.toUpperCase());
+}
+fetchedData(processData);
+
+// prototype
+
+const obj1 = {
+    name: 'John',
+    age: 30,
+    greet: function() {
+        console.log('Hello, my name is ' + this.name + ' and I am ' + this.age + ' years old.');
+    }
+};
+
+// Define obj2
+const obj2 = {
+  name: "Mathew",
+  age: 35,
+};
+
+// Connect obj1 properties and methods to obj2 using prototypes
+Object.setPrototypeOf(obj2, obj1);
+
+// Access properties and method of obj1 from obj2
+console.log(obj2.name); // Output: John
+console.log(obj2.age); // Output: 30
+obj2.greet(); // Output: Hello, my
+obj1.greet();
